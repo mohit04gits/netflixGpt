bc7b0a307dc1541a627e43c5af58827b093bfb3b
mohit04gits <mohitgupta1834@gmail.com>
2025-03-28 17:11:18 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,3 @@
-// import Header from "./Header";
-
-// const Login = () => {
-//   return (
-//     <div>
-//       <Header />
-//       <div>
-//         <img
-//         className="h-screen w-screen absolute"
-//           src="https://assets.nflxext.com/ffe/siteui/vlv3/fbf440b2-24a0-49f5-b2ba-a5cbe8ea8736/web/IN-en-20250324-TRIFECTA-perspective_d7c906ec-0531-47de-8ece-470d5061c88a_large.jpg"
-//           alt="Netflix Background"
-//         />
-//         <form className="flex flex-col p-10 w-3/12 items-center bg-black absolute ">
-//             <input type="text" placeholder="Email Address" className="p-2 m-2"/>
-//             <input type="text" placeholder="Email Address" className="p-2 m-2"/>
-//             <button className="p-4 m-4">Sign In</button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
 import { useState } from "react";
 import Header from "./Header";
 
